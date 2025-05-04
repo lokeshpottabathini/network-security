6872dbb23b07d1fa219bed9b8cd2b98758eca646
lokeshpottabathini <lokeshpottabathini@gmail.com>
2025-05-04 11:09:11 +0530

    data validation completed

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -35,16 +35,16 @@ DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
 """
 Data Validation related constant start with DATA_VALIDATION VAR NAME
 """
-# DATA_VALIDATION_DIR_NAME: str = "data_validation"
-# DATA_VALIDATION_VALID_DIR: str = "validated"
-# DATA_VALIDATION_INVALID_DIR: str = "invalid"
-# DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
-# DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
-# PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
 
-# """
-# Data Transformation related constant start with DATA_TRANSFORMATION VAR NAME
-# """
+"""
+Data Transformation related constant start with DATA_TRANSFORMATION VAR NAME
+"""
 # DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
 # DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
 # DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
